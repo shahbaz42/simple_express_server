adfaa531f20f4a0b76006249c9c3238b96ebc228
Shahbaz <shahbaz_ali@outlook.in>
2022-10-27 20:25:00 +0530

    add route to demonstrate query strings

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express()
 
 app.get("/", function(req, res){
-    res.send("OK, request recieved.")
+    var name = ""
+    res.send(`Your name is ${name}`)
+})
+
+app.get("/toUppercase", function(req, res){
+    console.log(req.query);
+    var name = req.query.name;
+    var movie = req.query.fav_movie;
+    name = name.toUpperCase()
+    res.send(`my name is ${name}, my favorite movie is ${movie}`);
 })
 
 app.listen(8000, function(){
