266043e58900cc13f84e19f45bea21fa0e43e0a3
Shahbaz <shahbaz_ali@outlook.in>
2022-10-27 20:52:13 +0530

    create a simple route for performing arithmetic operations

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ app.get("/toUppercase", function(req, res){
     res.send(`my name is ${name}, my favorite movie is ${movie}`);
 })
 
+
+app.get("/calculate/:operation", (req, res) => {
+    const opr = req.params.operation
+
+    if (opr == "add") {
+        // format of url : http://localhost:8000/calculate/add?num1=42&num2=42
+        const num1 = Number(req.query.num1)
+        const num2 = Number(req.query.num2)
+        const result = num1 + num2
+        res.send(result + "")
+    } else {
+        res.send("Operation not found");
+    }
+})
+
 app.listen(8000, function(){
     console.log("Server is up.");
 })
